a0d645d7f21e529173f9867e46238f22bf71a25a
xavi661 <javierpadin661@gmail.com>
2024-06-17 23:27:11 -0500

    la wea de nav jode

--- app/views/__includes/__main_content_header.php
+++ app/views/__includes/__main_content_header.php
@@ -1,28 +1,28 @@
-<?php include_once(__DIR__ . '/__header.php'); ?>
+<?php include_once (__DIR__ . '/__header.php'); ?>
 
-<?php include_once(__DIR__ . '/__nav.php'); ?>
+<?php include_once (__DIR__ . '/__nav.php'); ?>
 
-<?php include_once(__DIR__ . '/__aside.php'); ?>
+<?php include_once (__DIR__ . '/__aside.php'); ?>
 
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
   <!-- Content Header (Page header) -->
-  <div class="content-header">
+
+  <!-- <div class="content-header">
     <div class="container">
       <div class="row mb-2">
         <div class="col-sm-6">
-         <?php if(isset($data['pagina_titulo'])){ ?>
+         <?php if (isset($data['pagina_titulo'])) { ?>
             <h1 class="m-0"><?= $data['pagina_titulo'] ?></h1>
-          <?php }?>
+          <?php } ?>
         </div>
       </div>
     </div>
-    <!-- /.container-->
-  </div>
+    <!-- /.container
+  </div> -->
 
-  <?php include_once(__DIR__ . '/__alert.php'); ?>
+  <?php include_once (__DIR__ . '/__alert.php'); ?>
 
-   <!-- /.CONTENIDO PRINCIPAL -->
-  <section class="content">
-    <div class="container">
-     
+  <!-- /.CONTENIDO PRINCIPAL -->
+  <section>
+    <div>
