9d899a882bd97de7dd88bb1bef07751e19f46a89
xavi661 <javierpadin661@gmail.com>
2024-06-25 16:16:13 -0500

    Reapply "start project"
    This reverts commit 548a85e3b62cdc50cf54c8a630a7c6375bd4a9fd.

--- app/views/inicio.php
+++ app/views/inicio.php
@@ -1,6 +1,21 @@
 <?php include_once (__DIR__ . '/__includes/__main_content_header.php'); ?>
 
-<!-- TU CONTENIDO -->
+<!-- TU CONTENIDO Principal -->
+<div class="bgSecundarios_white p-5  w-100 h-100 principal">
+  <div class="row  bgSecundarios_white p-2 w-100 h-100 rounded">
+    <div class="form-group col-md-6 w-50" style="    display: flex;
+    flex-direction: column;
+    justify-content: center;">
+      <h2 class="textblue_primary mb-2">Bienvenido al Sistema Experto de Consultas Médicas</h2>
+      <p class="textSecundarios_black">Nuestro sistema experto utiliza inteligencia artificial para ofrecerte una
+        evaluación preliminar basada en tus síntomas, edad y peso, por favor responde algunas preguntas y recibe
+        recomendaciones generales de salud y posibles tratamientos.</p>
+      <h5 class="textblue_primary">Nota:</h5>
+      <p><i class="bi bi-check-lg fs-4"></i> Esto no sustituye una consulta médica profesional.</p>
+      <p><i class="bi bi-check-lg fs-4"></i> Medicamentos sin prescripción médica.</p>
+      <button class="button button_siguiente nav-link" style="width: 150px;" onclick="bienvenida()">
+        <h4 class="m-0">Empezar</h4>
+      </button>
 
 <!-- <link rel="stylesheet" href="<?= ASSETS ?>/dist/css/ApiMedic.css"> -->
 
