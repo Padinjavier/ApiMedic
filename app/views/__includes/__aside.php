770f2f0f439f4d023a4a27799d86d5687c872fad
xavi661 <javierpadin661@gmail.com>
2024-06-25 15:52:18 -0500

    la wea de nav jode

--- app/views/__includes/__aside.php
+++ app/views/__includes/__aside.php
@@ -6,7 +6,7 @@
         ?>
 
       <!-- Main Sidebar Container -->
-      <aside class="main-sidebar sidebar-dark-primary elevation-4">
+      <aside class="main-sidebar sidebar-dark-primary elevation-4" style="">
           <!-- Brand Logo -->
           <a href="index3.html" class="brand-link">
               <!-- <img
