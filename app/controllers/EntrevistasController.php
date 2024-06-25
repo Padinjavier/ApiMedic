f3bd5148c31f6bdbfd111e018304bda911e6e0e4
xavi661 <javierpadin661@gmail.com>
2024-06-25 11:11:24 -0500

    listo paso de datos

--- app/controllers/EntrevistasController.php
+++ app/controllers/EntrevistasController.php
@@ -5,15 +5,48 @@ class EntrevistasController extends Controller
     function index()
     {
         $data['page_title'] = "Entrevistas";   
-        foreach ($_SESSION['sintomas'] as $key => $value) {
-            $data['sintomas'][$key] = $value;
+        // Verificar si la sesión de síntomas está definida y tiene datos
+        if (isset($_SESSION['sintomas']) && !empty($_SESSION['sintomas'])) {
+            foreach ($_SESSION['sintomas'] as $key => $value) {
+                $data['sintomas'][$key] = $value;
+            }
+        } else {
+            $data['sintomas'] = [];
         }
+
         $data['irmedico'] = isset($_SESSION['irmedico']) ? $_SESSION['irmedico'] : '';
- 
+
+        $this->view("entrevistas", $data);
         // Recuperar otras variables según sea necesario
          
         $this->view("entrevistas", $data);
     }
-
+    function store()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Inicializar la sesión si no está ya iniciada
+            if (session_status() == PHP_SESSION_NONE) {
+                session_start();
+            }
+    
+            // Limpiar la variable de sesión para evitar duplicados
+          
+    
+            // Verificar si se han enviado síntomas
+            if (isset($_POST['sintomas2']) && is_array($_POST['sintomas2'])) {
+                // Recorrer el array de 'sintomas[]' y guardarlos en la sesión
+                foreach ($_POST['sintomas2'] as $sintoma) {
+                    $_SESSION['sintomas2'][] = htmlspecialchars($sintoma);
+                }
+            }
+    
+            
+    
+            // Redirigir a la vista de síntomas
+            header('Location: ' . ROOT . 'resultados');
+            exit();
+        }
+    }
+    
    
 }
--- app/controllers/ResultadosController.php
+++ app/controllers/ResultadosController.php
@@ -4,7 +4,15 @@ class ResultadosController extends Controller
 
     function index()
     {
-        $data['page_title'] = "resultados";      
+        $data['page_title'] = "resultados";  
+        
+
+        foreach ($_SESSION['sintomas2'] as $key => $value) {
+            $data['sintomas2'][$key] = $value;
+        }
+
+       
+
         $this->view("resultados", $data);
     }
 
